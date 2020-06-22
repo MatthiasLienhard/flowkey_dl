12d241982dcedb1532abe4abb412c63d7fa35ec6
MatthiasLienhard <mali270484@gmail.com>
2020-06-22 03:48:47 +0200

    add functions

--- flowkey_dl.py
+++ flowkey_dl.py
@@ -3,7 +3,7 @@ import requests
 
 import imageio
 
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
 import numpy as np
 import os
@@ -26,42 +26,92 @@ def flowkey_dl(url):
     r,g,b,a=np.rollaxis(imgs_comb, axis=-1)
     return 255-a
 
-def find_measure(image):
+def find_measure(image, min_sz=100):
     #image is a numpy array
     #there are about 20 pixel above and 15 below the lines. 
     # at most 5 pixels can be brighter than 100
-    measures=np.where((image>100)[50:-50,:].sum(0)<10) 
-    return next(iter(measures))
-    #todo: minimal distance: 200 pixel
+    positions=np.where((image>100)[50:-50,:].sum(0)<10)[0]
+    measures=[positions[0]]
+    for i in positions:
+        if i> measures[-1]+min_sz:
+            measures.append(i)
+    print(f'found {len(measures)-1} measures')
+    return measures
 
-def arange_image(image=None, title=None, autor=None,width=2480, height=3508, scale=1):
-    out=Image.fromarray(255*np.ones((int(height),int(width))))
+def parse_nums(val=None):
+    #parse strings like '1,3,6-10,15'
+    nums=[]
+    if val is not None and val:
+        for v in val.split(','):
+            if '-' in v:
+                _from, to= v.split('-')
+                nums.extend(list(range(int(_from), int(to)+1)))
+            else:
+                nums.append(int(v))
+    return nums
+
+
+def arange_image(image=None, title='', author='',width=2480, height=3508, scale=1,space=50, sel_measures=None,break_measures=None, nobreak_measures=None,font_size=(40,20) ):
+    mar=20
+    sel_measures,break_measures, nobreak_measures=[parse_nums(val) for val in (sel_measures,break_measures, nobreak_measures)]
+    out=[Image.fromarray(255*np.ones((int(height),int(width))))]
+    fnt = [ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', sz) for sz in font_size]
+    d = ImageDraw.Draw(out[-1])
+    w, h = d.textsize(title, font=fnt[0])
+    d.text(((width-w)/2,mar),title, font=fnt[0], fill=0)
+    w2, h2= d.textsize(author, font=fnt[1])
+    d.text((width-mar-w2,mar+h), author, font=fnt[1], fill=0)
+
+    print(f'arage images of size {width}x{height}')
     if image is None:
         return out
     measures=find_measure(image)
-    offset=0
+    if sel_measures:
+        print(f'selecting measures {sel_measures}')
+        image=np.hstack([image[:,measures[m-1]:measures[m]] for m in sel_measures])
+        #offset=0
+        rm=[0]
+        #new_measures=list()
+        for i,m in enumerate(measures[1:]):
+            rm.append(rm[i])
+            if i+1 not in sel_measures:
+                #offset+=measures[i]-m
+                rm[i+1]+=1
+        nobreak_measures=[v-rm[v] for v in nobreak_measures if v in sel_measures]
+        break_measures=[v-rm[v] for v in break_measures if v in sel_measures]
+        measures=find_measure(image)
+
+    
+    offset=measures[0]
     breaks=list()
     for i,ix in enumerate(measures):
-        if ix-offset>width/scale:
+        if i not in nobreak_measures and (ix-offset>(width-2*mar)/scale or i in break_measures):
             if measures[i-1]>offset:
                 breaks.append(measures[i-1])
             else:
                 breaks.append(measures[i])
             offset=breaks[-1]
     breaks.append(image.shape[1])
-    offset=0
-    y=0
+    offset=measures[0]
+    y=int(mar+h+h2+space/2)
     for i,ix in enumerate(breaks):
         print(f'{offset}, {ix}')
+        if y>height-mar:
+            out.append(Image.fromarray(255*np.ones((int(height),int(width)))))
+            y=mar
         patch=image[:,offset:ix+10]
         dim=patch.shape
         patch=Image.fromarray(patch)
         patch=patch.resize((int(x*scale) for x in reversed(dim)))
-        out.paste(patch, (0,y)) 
-        y+=patch.height+10
+        out[-1].paste(patch, (mar,y)) 
+        y+=patch.height+space
+
         offset=ix-10    
     return out
 
+def load_image(filename):
+    img=PngImageFile(filename)
+    return(img)
 
 
 def save_image(image, filename, author, title):
--- flowkey_dl_gui.py
+++ flowkey_dl_gui.py
@@ -1,65 +1,153 @@
 #!/bin/python3
 
 from tkinter import *
+from tkinter import ttk
 import sys
 from flowkey_dl import flowkey_dl, arange_image
 import os
 from PIL import ImageTk
 
-class downloadWindow(object):
-    def __init__(self,master):
-        top=self.top=Toplevel(master)
-        self.l=Label(top,text="Enter Download Link")
-        self.l.pack()
-        self.e = Entry(top)
-        self.e.pack()
-        self.b=Button(top,text='Download',command=self.download)
-        self.b.pack()
-    def download(self):
-        url=self.e.get()
-        self.image=flowkey_dl(url)
-        self.top.destroy()
+dim={'A4 Landscape':(1169,826),'A4 Portrait':(826,1169)}
 
-class mainWindow(object):
+    
+class MainWindow(object):
     def __init__(self,master):
         self.master=master
-        self.b=Button(master,text="download",command=self.download)
-        self.b.grid(row=0,column=0,rowspan=2)
-        self.title_l=Label(master,text="Title")
-        self.title_l.grid(row=0,column=1)
-        self.title=Entry(master)
-        self.title.grid(row=1,column=1)
-        self.artist_l=Label(master,text="Artist/Author/Composer")
-        self.artist_l.grid(row=0,column=2)
-        self.artist=Entry(master)
-        self.artist.grid(row=1,column=2)
-        self.b2=Button(master,text="preview",command=self.arange_image)
-        self.b2.grid(row=0,column=3, rowspan=2)
-        self.scale = Scale(master, from_=10, to=200, orient=HORIZONTAL)
-        self.scale.grid(row=2,column=0,columnspan=4)
-        self.width=2480/4
-        self.height=3508/4
-        self.canvas = Canvas(master,width=self.width,height=self.height, bg='white') #100 dpi
-        self.img=ImageTk.PhotoImage(arange_image(width=self.width, height=self.height))
-        self.img_area=self.canvas.create_image(0,0,anchor=NW, image=self.img)
-        self.canvas.grid(row=3,column=0,columnspan=4)
+        self.song=Frame(master)
+        self.song.grid(row=0,column=0)
+        self.layout=Frame(master)
+        self.layout.grid(row=1,column=0)
+        self.measure=Frame(master)
+        self.measure.grid(row=2, column=0)
+        self.preview=Frame(master)
+        self.preview.grid(row=0,column=1,rowspan=3)
+        song=self.song
+        self.song_lab=Label(song, text='Song:')
+        self.song_lab.grid(row=0,column=0)
+        self.title_l=Label(song,text="Title")
+        self.title_l.grid(row=1,column=0)
+        self.title=Entry(song)
+        self.title.grid(row=1,column=1,columnspan=2)
+        self.artist_l=Label(song,text="Artist/Author/Composer")
+        self.artist_l.grid(row=2,column=0)
+        self.artist=Entry(song)
+        self.artist.grid(row=2,column=1,columnspan=2)
+        self.url_l=Label(song,text="url")
+        self.url_l.grid(row=3,column=0)
+        self.url=Entry(song)
+        self.url.grid(row=3,column=1,columnspan=2)
+        self.download_b=Button(song,text="download",command=self.download)
+        self.download_b.grid(row=4,column=0)
+        self.load_b=Button(song,text="load",command=self.load)
+        self.load_b.grid(row=4,column=1)
+        self.save_b=Button(song,text="save",command=self.save)
+        self.save_b.grid(row=4,column=2)
+        layout=self.layout
+        self.layout_lab=Label(layout, text='Layout:')
+        self.layout_lab.grid(row=0, column=0)
+        self.size_lab=Label(layout, text='Page size')
+        self.size_lab.grid(row=1, column=0)
+        self.size=ttk.Combobox(layout,state='readonly', values=list(dim))
+        self.size.set('A4 Portrait')
+        self.size.grid(row=1,column=1, columnspan=2)
+        self.width,self.height=dim[self.size.get()]        
+        self.scale_lab=Label(layout, text='sheet scale')
+        self.scale_lab.grid(row=2,column=0)
+        self.scale = Scale(layout, from_=10, to=200, orient=HORIZONTAL)
+        self.scale.set(100)
+        self.scale.grid(row=2,column=1,columnspan=2)
+        self.prev_scale_lab=Label(layout, text='preview scale')
+        self.prev_scale_lab.grid(row=3,column=0)
+        self.prev_scale = Scale(layout, from_=10, to=100, orient=HORIZONTAL)
+        self.prev_scale.set(50)
+        self.prev_scale.grid(row=3,column=1,columnspan=2)   
+        self.prev_width=self.width*self.prev_scale.get()/100     
+        self.prev_height=self.height*self.prev_scale.get()/100     
+        self.font_sz_lab=Label(layout, text='Font size')
+        self.font_sz_lab.grid(row=4,column=0)
+        self.font_sz_title=ttk.Combobox(layout,state='readonly', values=list(range(10,100)))
+        self.font_sz_title.set(50)
+        self.font_sz_title.grid(row=4,column=1)
+        self.font_sz_author=ttk.Combobox(layout,state='readonly', values=list(range(10,100)))
+        self.font_sz_author.set(30)
+        self.font_sz_author.grid(row=4,column=2)
+        self.space_lab=Label(layout,text='Spacing')
+        self.space_lab.grid(row=5, column=0)
+        self.space=Scale(layout, from_=10, to=200, orient=HORIZONTAL)
+        self.space.set(50)
+        self.space.grid(row=5,column=1, columnspan=2)
+        measure=self.measure
+        self.measure_lab=Label(measure, text='Measure')
+        self.measure_lab.grid(row=0,column=0, columnspan=2)
+        self.measure_sel_lab=Label(measure, text='select')
+        self.measure_sel_lab.grid(row=1,column=0)
+        self.measure_sel=Entry(measure)
+        self.measure_sel.grid(row=1,column=1)
+        self.measure_pre_lab=Label(measure, text='prevent break')
+        self.measure_pre_lab.grid(row=2,column=0)
+        self.measure_pre=Entry(measure)
+        self.measure_pre.grid(row=2,column=1)
+        self.measure_force_lab=Label(measure, text='force break')
+        self.measure_force_lab.grid(row=3,column=0)
+        self.measure_force=Entry(measure)
+        self.measure_force.grid(row=3,column=1)
+        preview=self.preview
+        self.image=None
+        self.current_page=0
+        self.canvas = Canvas(preview,width=self.prev_width,height=self.prev_height, bg='white') #100 dpi
+        self.prev_img=[ImageTk.PhotoImage(i) for i in arange_image(width=self.prev_width, height=self.prev_height)]
+        self.prev_area=self.canvas.create_image(0,0,anchor=NW, image=self.prev_img[self.current_page])
+        self.canvas.grid(row=0,column=0, columnspan=3)
+        self.pre_b=Button(preview, text='<',command=self.pre_img)
+        self.pre_b.grid(row=1, column=0)
+        self.preview_lab=Label(preview, text=f'Page {self.current_page}/{len(self.prev_img)}')
+        self.preview_lab.grid(row=1,column=1)
+        self.next_b=Button(preview, text='>',command=self.next_img)
+        self.next_b.grid(row=1, column=2)
 
+    
     def download(self):
-        self.w=downloadWindow(self.master)
-        self.b["state"] = "disabled" 
-        self.master.wait_window(self.w.top)
-        self.b["state"] = "normal"
+        url=self.url.get()
+        self.image=flowkey_dl(url)
+        self.get_preview()
 
-    def arange_image(self):
-        self.img=ImageTk.PhotoImage(arange_image(self.w.image, self.title.get(), self.artist.get(),self.width, self.height, scale=self.scale.get()/100))
-        print('got an image!')
+    def save(self):
+        print('not implemented yet... instead we make new preview')
+        self.get_preview()
+        
+    def load(self):        
+        pass
+
+    def pre_img(self):
+        self.current_page-=1
+        self.preview_lab.config(text=f'Page {self.current_page}/{len(self.prev_img)}')
+        self.canvas.itemconfig(self.prev_area, image = self.prev_img[self.current_page])
+
+    def next_img(self):
+        self.current_page+=1
+        self.preview_lab.config(text=f'Page {self.current_page}/{len(self.prev_img)}')
+        self.canvas.itemconfig(self.prev_area, image = self.prev_img[self.current_page])
+
+    def get_preview(self):
+        self.width,self.height=dim[self.size.get()]     
+        self.prev_width=int(self.width*self.prev_scale.get()/100     )
+        self.prev_height=int(self.height*self.prev_scale.get()/100     )
+        self.canvas.config(width=self.prev_width, height=self.prev_height)
+        self.prev_img=[ImageTk.PhotoImage(i.resize((self.prev_width, self.prev_height))) for i in arange_image(self.image, 
+                            self.title.get(), self.artist.get(),self.width, self.height, 
+                            scale=self.scale.get()/100,
+                            space=int(self.space.get()),
+                            sel_measures=self.measure_sel.get(), 
+                            break_measures=self.measure_force.get(), 
+                            nobreak_measures= self.measure_pre.get() ,
+                            font_size=(int(self.font_sz_title.get()),int(self.font_sz_author.get())))]
         #self.canvas.create_image(0,0,anchor=NW,image=ImageTk.PhotoImage(image))
-        self.canvas.itemconfig(self.img_area, image = self.img)
+        self.canvas.itemconfig(self.prev_area, image = self.prev_img[self.current_page])
        
 
 
 
 if __name__ == "__main__":
     root=Tk()
-    m=mainWindow(root)
+    m=MainWindow(root)
     root.mainloop()
