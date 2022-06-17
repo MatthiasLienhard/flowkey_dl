48db708d421585599c12b179c84278c1517b02b4
Matthias Lienhard <lienhard@molgen.mpg.de>
2022-06-17 17:10:40 +0200

    fixed missing explort of save_pdf

--- src/flowkey_dl/__init__.py
+++ src/flowkey_dl/__init__.py
@@ -4,6 +4,6 @@ except ModuleNotFoundError:
     from importlib_metadata import distribution  # py3.7
 __version__ = distribution('flowkey-dl').version
 from .flowkey_dl import flowkey_dl, find_measure, parse_nums, arange_image
-from .flowkey_dl import load_image, strip_url, make_url, save_png
+from .flowkey_dl import load_image, strip_url, make_url, save_png, save_pdf
 __all__ = ['flowkey_dl', 'find_measure', 'parse_nums', 'arange_image',
            'load_image', 'strip_url', 'make_url', 'save_png']
