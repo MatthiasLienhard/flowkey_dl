7ee48a97954efd183d2dd8a6fa8175562ba9a952
gml1992 <gracemarielowe@gmail.com>
2024-05-17 14:30:05 -0500

    flowkey-dl-cli <URL> [<PDF-Output-Path>] [-t <title>] [-a <artist>]

--- src/flowkey_dl/__init__.py
+++
@@ -1,9 +0,0 @@
-try:
-    from importlib.metadata import distribution
-except ModuleNotFoundError:
-    from importlib_metadata import distribution  # py3.7
-__version__ = distribution('flowkey-dl').version
-from .flowkey_dl import flowkey_dl, find_measure, parse_nums, arange_image
-from .flowkey_dl import load_image, strip_url, make_url, save_png, save_pdf
-__all__ = ['flowkey_dl', 'find_measure', 'parse_nums', 'arange_image',
-           'load_image', 'strip_url', 'make_url', 'save_png']
